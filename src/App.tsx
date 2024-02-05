9dc493e2d0b2f77de58434759d777270d7fb4283
Fahim1837 <fahimrb37@gmail.com>
2024-02-05 10:20:16 +0600

    Styled Side Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import NavBar from "./components/navigation/NavBar"
+import SideBar from "./components/side bar/SideBar"
 
 function App() {
   	return (
 		<>
    		<div className="grid grid-cols-8 laptop:grid-cols-6 tablet:grid-cols-4  gap-[10px]">
 			<NavBar/>
-			<div className="col-span-1 bg-slate-300">Home About Icon</div>
+			<div className="col-span-1 bg-slate-300"><SideBar/></div>
 			<div className="col-span-2 bg-slate-400 tablet:hidden">Categories</div>
 			<div className="col-span-3 bg-slate-500">Main Section</div>
 			<div className="col-span-2 bg-slate-600 laptop:hidden">Side Settings</div>
---
+++ src/components/side bar/SideBar.tsx
@@ -0,0 +1,33 @@
+import { CiHome } from "react-icons/ci"
+import { FaRegBookmark } from "react-icons/fa6"
+import { IoMdBook } from "react-icons/io"
+import { IoBulbOutline } from "react-icons/io5"
+import { PiChatsTeardropThin } from "react-icons/pi"
+import { RiApps2Line, RiMedicineBottleLine } from "react-icons/ri"
+import Logo from "../navigation/Logo"
+import Donate from "./Donate"
+
+function SideBar() {
+    const icons = [
+    <CiHome size={20}/>,
+    <RiApps2Line  size={20}/>,
+    <IoBulbOutline size={20}/>,
+    <FaRegBookmark size={20}/>,
+    <PiChatsTeardropThin size={20}/>,
+    <RiMedicineBottleLine size={20}/>,
+    <IoMdBook  size={20}/>
+]
+  return (
+    <div className="flex flex-col gap-36 items-center m-8">
+        <Logo/>
+        <ul className="flex flex-col gap-7">
+            {icons.map((item, index) => <li><button className="bg-slate-200 p-2 rounded-full" key={index}>{item}</button></li>)}
+        </ul>
+        <div className="mt-5">
+            <Donate />
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
