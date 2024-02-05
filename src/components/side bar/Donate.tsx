62c599be376198d3bf0711c18418b92be0fb01b6
Fahim1837 <fahimrb37@gmail.com>
2024-02-05 10:18:19 +0600

    Created and Styled Donate

---
+++ src/components/side bar/Donate.tsx
@@ -0,0 +1,9 @@
+import { BiSolidDonateHeart } from "react-icons/bi"
+
+function Donate() {
+  return (
+    <button className="p-4 rounded-[10px] bg-[#1FA45B]"><BiSolidDonateHeart size={24} color="white" /></button>
+  )
+}
+
+export default Donate
